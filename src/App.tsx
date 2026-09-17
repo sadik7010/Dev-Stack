90e6dbf55732f22649fb488c6787d91bd2cdcff9
Mahfuz Sadik <s45776616@gmail.com>
2026-09-17 21:34:40 +0600

    Hero Section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import Hero from "./Hero";
 import Navbar from "./Navbar";
-
 
 const App = () => {
   return (
-    
-      <Navbar/>
-    
+    <>
+      <Navbar />
+      <Hero />
+    </>
   );
 };
 
